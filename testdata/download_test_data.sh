ae9008fe74163f7cde771a1ff37424ca591e54a2
s-andrews <simon@proteo.me.uk>
2023-10-27 22:01:38 +0100

    Added shebang to download script

---
+++ testdata/download_test_data.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://www.dfam.org/releases/Dfam_3.7/annotations/hg38/hg38.nrph.hits.gz
+wget https://www.dfam.org/releases/Dfam_3.7/families/Dfam_curatedonly.embl.gz
+wget https://www.dfam.org/releases/ref-genomes/hg38/dfamseq
